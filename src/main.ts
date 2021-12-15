fc5705379f005eb720f9c8336cd6f1132d692033
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-12-15 20:39:57 +0700

    feat: add nprogress

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { createApp } from 'vue/dist/vue.esm-bundler';
 import { createHead } from '@vueuse/head';
 import { router } from '@/router/routes';
 
+import 'nprogress/nprogress.css';
 import 'virtual:windi.css';
+import '@/assets/style.css';
 
 const app = createApp({});
 const head = createHead();
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import NProgress from 'nprogress';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -7,17 +9,23 @@ const router = createRouter({
       path: '/',
       alias: '/home',
       component: () => import('./pages/index.vue'),
-      meta: {
-        title: 'Modern Favicon Generator — Fav',
-      },
     },
     {
       path: '/about',
       component: () => import('./pages/about.vue'),
-      meta: {
-      },
     },
   ],
+});
+
+router.beforeEach(() => {
+  NProgress.start();
+});
+
+router.onError(() => {
+  NProgress.done();
+});
+router.afterEach(() => {
+  NProgress.done();
 });
 
 export { router };
