3c560d0d992198072ead1fe5c9ba8925b26ae5e1
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-12-18 20:39:26 +0700

    test: add UploadBox component test

---
+++ src/components/tests/UploadBox.test.ts
@@ -0,0 +1,19 @@
+import { mount } from '@vue/test-utils';
+
+import UploadBox from '@/components/UploadBox.vue';
+
+describe('<UploadBox>', () => {
+  it('should display an upload file box', () => {
+    const wrapper = mount(UploadBox);
+
+    const input = wrapper.find('input[type=file]');
+    const label = wrapper.find('label[for=image-file]');
+
+    expect(input.exists()).toBe(true);
+    expect(label.exists()).toBe(true);
+
+    expect(label.find('svg').exists()).toBe(true);
+  });
+
+  // TODO: add more test if the mocking is sane
+});
