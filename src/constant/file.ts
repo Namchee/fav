b98fc9cc6fe7e6dbc96e83af9198d2cc110b85eb
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-12-23 19:56:15 +0700

    fix: include webp for input file accept

--- src/constant/file.ts
+++ src/constant/file.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line max-len
-export const ACCEPTED_FILES = 'image/png, image/jpeg, image/x-icon, image/vnd-microsoft-icon, image/svg+xml';
+export const ACCEPTED_FILES = 'image/png, image/jpeg, image/x-icon, image/vnd-microsoft-icon, image/svg+xml, image/webp';
 export const FILE_SIZE = 10 * 1024 * 1024;
 export const URL_EXPIRE = 250;
--- src/constant/platform.ts
+++ src/constant/platform.ts
@@ -1,7 +1,5 @@
 /* eslint-disable max-len */
 
-import { IconKey, Favicon } from '@/types';
-
 export const PLATFORM_LIST = [
   {
     name: 'Legacy',
@@ -25,38 +23,3 @@ export const PLATFORM_LIST = [
     value: 'apple',
   },
 ];
-
-export const PLATFORM_ICONS: Record<IconKey, Favicon[]> = {
-  legacy: [
-    {
-      name: 'favicon.ico',
-      mime: 'image/x-icon',
-      size: 32,
-    },
-  ],
-  modern: [
-    {
-      name: 'icon.svg',
-      mime: 'image/svg+xml',
-    },
-  ],
-  android: [
-    {
-      name: '192.png',
-      mime: 'image/png',
-      size: 192,
-    },
-    {
-      name: '512.png',
-      mime: 'image/png',
-      size: 512,
-    },
-  ],
-  apple: [
-    {
-      name: 'apple-touch-icon.png',
-      mime: 'image/png',
-      size: 180,
-    },
-  ],
-};
